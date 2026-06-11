90af3c6d909018d2dc76aee0fc7514466acb3995
thxCode <thxcode0824@gmail.com>
2026-06-11 10:32:47 +0800

    refactor: use new nfd chart
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- pkg/worker/kuberess/apps_node_feature_discovery.go
+++ pkg/worker/kuberess/apps_node_feature_discovery.go
@@ -27,7 +27,7 @@ func installNodeFeatureDiscovery(ctx context.Context, helmCli *helm.Client, glob
 
 	release := "gpustack-node-feature-discovery"
 	path := filepath.Join(system.SubConfDir("charts"), fmt.Sprintf("%s-%s.tgz", name, version))
-	download := fmt.Sprintf("https://github.com/kubernetes-sigs/node-feature-discovery/releases/download/v%[1]s/node-feature-discovery-chart-%[1]s.tgz", version) // nolint:lll
+	download := fmt.Sprintf("https://thxcode.github.io/node-feature-discovery/charts/node-feature-discovery-%[1]s.tgz", version)
 
 	valuesContext := globalValuesContext
 	valuesContext["Release"] = release
@@ -90,6 +90,9 @@ master:
        matchLabels:
          kubernetes.io/metadata.name: "{{ $.Namespace }}"
 
+{{- $pciClassPrefixes := getPciClassPrefixes }}
+{{- $pciVendorIDs := getPciVendorIDs }}
+
 worker:
   enable: true
   tolerations:
@@ -102,8 +105,6 @@ worker:
     create: true
     annotations:
       {{ $.ManagedLabel }}: "true"
-{{- $pciClassPrefixes := getPciClassPrefixes }}
-{{- $pciVendorIDs := getPciVendorIDs }}
   config:
     core:
       labelSources:
@@ -119,22 +120,6 @@ worker:
 {{- end }}
         deviceLabelFields:
           - vendor
-      custom:
-        - name: "has acceleratable devices"
-          vars:
-            has-acceleratable-devices: "true"
-          matchFeatures:
-            - feature: pci.device
-              matchExpressions:
-                class: {op: InRegexp, value: [{{ range $i, $c := $pciClassPrefixes }}{{ if $i }}, {{ end }}{{ printf "^%s" $c | quote }}{{ end }}]}
-                vendor: {op: In, value: {{ toJson $pciVendorIDs }}}
-        - name: "hasn't acceleratable devices"
-          labels:
-            "feature.gpustack.ai/acceleratable": "false"
-          matchFeatures:
-            - feature: rule.matched
-              matchExpressions:
-                has-acceleratable-devices: {op: DoesNotExist}
 
 topologyUpdater:
   enable: false
@@ -154,6 +139,51 @@ gc:
 
 prometheus:
   enable: false
+
+nodeFeatureRule:
+  create: true
+  name: "gpustack-cpu-info"
+  rules:
+  - name: "attached cpu info to annotations"
+    annotations:
+      feature.gpustack.ai/cpu-name: "@cpu.model.name"
+      feature.gpustack.ai/cpu-family: "@cpu.model.family"
+      feature.gpustack.ai/cpu-physical-cores: "@cpu.model.physical_cores"
+      feature.gpustack.ai/cpu-threads-per-core: "@cpu.model.threads_per_core"
+      feature.gpustack.ai/cpu-logical-cores: "@cpu.model.logical_cores"
+      feature.gpustack.ai/cpu-stepping: "@cpu.model.stepping"
+      feature.gpustack.ai/cpu-cache-line: "@cpu.model.cache_line"
+      feature.gpustack.ai/cpu-hz: "@cpu.model.hz"
+      feature.gpustack.ai/cpu-boost-freq: "@cpu.model.boost_freq"
+      feature.gpustack.ai/cpu-cache-l1i: "@cpu.model.cache_l1i"
+      feature.gpustack.ai/cpu-cache-l1d: "@cpu.model.cache_l1d"
+      feature.gpustack.ai/cpu-cache-l2: "@cpu.model.cache_l2"
+      feature.gpustack.ai/cpu-cache-l3: "@cpu.model.cache_l3"
+    matchFeatures:
+    - feature: cpu.model
+      matchExpressions:
+        vendor_id: 
+          op: Exists
+  - name: "detect whether the node has acceleratable devices"
+    vars:
+      has-acceleratable-devices: "true"
+    matchFeatures:
+    - feature: pci.device
+      matchExpressions:
+        class:
+          op: InRegexp
+          value: [{{ range $i, $c := $pciClassPrefixes }}{{ if $i }},{{ end }}{{ printf "^%s" $c | quote }}{{ end }}]
+        vendor: 
+          op: In
+          value: {{ toJson $pciVendorIDs }}
+  - name: "identify the node without any acceleratable device"
+    labels:
+      feature.gpustack.ai/acceleratable: "false"
+    matchFeatures:
+    - feature: rule.matched
+      matchExpressions:
+        has-acceleratable-devices: 
+          op: DoesNotExist
 `
 
 func getNfdChartTemplateValues(name string, data map[string]any, extendFuncMap template.FuncMap) helm.TemplateValues {
