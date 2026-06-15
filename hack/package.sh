0ed6b3b4b047468e748214fa5a2ad9ba7dc96371
thxCode <thxcode0824@gmail.com>
2026-06-15 16:50:55 +0800

    fix: force per-commit rebuild of gpustack-operator image
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- hack/package.sh
+++ hack/package.sh
@@ -72,6 +72,7 @@ function pack() {
       --builder "gpustack" \
       --platform "linux/${PACKAGE_ARCH}" \
       --tag "${tag}" \
+      --build-arg "GPUSTACK_GIT_COMMIT=$(git rev-parse HEAD 2>/dev/null || echo "unknown")" \
       --file "${ROOT_DIR}/pack/${task}/Dockerfile" \
       --ulimit nofile=65536:65536 \
       --shm-size 16G \
